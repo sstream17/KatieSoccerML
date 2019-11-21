ec91a0436d8fb80f141bf2134c8afe90fe5b37bb
Spencer Stream <sprewstream@gmail.com>
2019-11-21 10:35:45 -0600

    Updated to allow Agents to handle resetting themselves. Max step logic has been removed from the Academy script and the Max Step property for each agent has been set.

--- Katie Soccer ML/Assets/Scripts/KatieSoccerAcademy.cs
+++ Katie Soccer ML/Assets/Scripts/KatieSoccerAcademy.cs
@@ -12,32 +12,4 @@ public class KatieSoccerAcademy : Academy
 	public float spawnAreaMarginMultiplier;
 
     public float TimePenalty = 500f;
-
-    public int MaxSteps = 7500;
-
-    private GameObject[] agents;
-
-    public override void AcademyReset()
-    {
-        base.AcademyReset();
-
-        agents = GameObject.FindGameObjectsWithTag("Agent");
-
-        foreach (var agentGameObject in agents)
-        {
-            KatieSoccerAgent agent = agentGameObject.GetComponent<KatieSoccerAgent>();
-            agent.Done();
-        }
-    }
-
-    public override void AcademyStep()
-    {
-        base.AcademyStep();
-
-        var steps = GetStepCount();
-        if (steps % MaxSteps == 0)
-        {
-            AcademyReset();
-        }
-    }
 }
